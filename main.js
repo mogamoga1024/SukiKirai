dddf95bf8f74095928d2b126e09c6cb45ca6e057
mogamoga <mogamoga.g1thu6@gmail.com>
2023-07-26 00:24:54 +0900

    1

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
 const $html = $("html");
+const $body = $("body");
 
 $html.on("click contextmenu", function(e) {
     // 左クリック
     if (e.which === 1) {
-        $html.css("background-color", "#f00");
+        // $body.css("background-color", "#f00");
     }
     // 右クリック
     else if (e.which === 3) {
-        $html.css("background-color", "#00f");
+        // $body.css("background-color", "#00f");
     }
     return false;
 });
