faa9e676ccbced9d187982388177ade0a4525e96
mogamoga <mogamoga.g1thu6@gmail.com>
2023-07-26 01:22:18 +0900

    るー！ぱー！

--- main.js
+++ main.js
@@ -4,32 +4,52 @@ const $body = $("body");
 
 const kiraiHue = 240;
 const sukiHue = 330;
-let hue = (kiraiHue + sukiHue) / 2;
 
-$bodySetBackgroundColor(hue);
+const minKoukando = 0;
+const maxKoukando = 10;
+let koukando = Math.floor(maxKoukando / 2);
+
+$bodySetBackgroundColor(koukando);
 
 $html.on("click contextmenu", function(e) {
     // 左クリック
     // 好きゲージが増える
     if (e.which === 1) {
-        hue = Math.min(hue + 10, sukiHue);
+        koukando = Math.min(koukando + 1, maxKoukando);
     }
     // 右クリック
     // 嫌いゲージが増える
     else if (e.which === 3) {
-        hue = Math.max(hue - 10, kiraiHue);
+        koukando = Math.max(koukando - 1, minKoukando);
     }
     else {
         return;
     }
 
-    $bodySetBackgroundColor(hue);
+    $bodySetBackgroundColor(koukando);
 
     return false;
 });
 
-function $bodySetBackgroundColor(hue) {
+function $bodySetBackgroundColor(koukando) {
+    const hue = koukandoToHue(koukando);
     const hsl = `hsl(${hue}, 100%, 80%)`;
     $body.css("background-color", hsl);
+}
+
+function koukandoToHue(koukando) {
+    if (koukando < minKoukando || koukando > maxKoukando) {
+        throw new Error("好感度が範囲外")
+    }
+
+    if (koukando === minKoukando) {
+        return kiraiHue;
+    }
+    else if (koukando === maxKoukando) {
+        return sukiHue;
+    }
+    else {
+        return kiraiHue + (sukiHue - kiraiHue) * (koukando / (maxKoukando - minKoukando));
+    }
 }
 
